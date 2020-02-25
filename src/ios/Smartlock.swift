d5baac6d9685777c0cce8b8cb9d5c4fe85d37fd7
Rubén Panadero <rpanadero@okode.com>
2020-02-25 11:08:49 +0100

    Implementing smarlock on ios

---
+++ src/ios/Smartlock.swift
@@ -0,0 +1,21 @@
+import Foundation
+import LocalAuthentication
+
+
+class Smartlock : CDVPlugin {
+
+    override func pluginInitialize() {
+        super.pluginInitialize()
+    }
+
+    func save(_ command: CDVInvokedUrlCommand){
+
+    }
+
+    func request(_ command: CDVInvokedUrlCommand){
+    }
+
+    func delete(_ command: CDVInvokedUrlCommand){
+    }
+
+}
