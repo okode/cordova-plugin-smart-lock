8b5e9e8d088d018bf1616eb91141b3b5549e52c3
Jorge Navarro <jnavarro@okode.com>
2020-02-26 10:05:41 +0100

    Update types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,69 +1,66 @@
-declare module SmartlockPlugin {
+interface SmartlockPlugin {
+  readonly SMARTLOCK__REQUEST__ACCOUNTS_NOT_FOUND;
+  readonly SMARTLOCK__REQUEST__DIALOG_CANCELLED;
+  readonly SMARTLOCK__SAVE;
+  readonly SMARTLOCK__SAVE__BAD_REQUEST;
+  readonly SMARTLOCK__DELETE;
+  readonly SMARTLOCK__COMMON__UNKOWN;
+  readonly SMARTLOCK__COMMON__CONCURRENT_NOT_ALLOWED;
+  readonly SMARTLOCK__COMMON__GOOGLE_API_UNAVAILABLE;
+  readonly SMARTLOCK__COMMON__RESOLUTION_PROMPT_FAIL;
 
-  interface SmartlockPluginStatic {
-    readonly SMARTLOCK__REQUEST__ACCOUNTS_NOT_FOUND;
-    readonly SMARTLOCK__REQUEST__DIALOG_CANCELLED;
-    readonly SMARTLOCK__SAVE;
-    readonly SMARTLOCK__SAVE__BAD_REQUEST;
-    readonly SMARTLOCK__DELETE;
-    readonly SMARTLOCK__COMMON__UNKOWN;
-    readonly SMARTLOCK__COMMON__CONCURRENT_NOT_ALLOWED;
-    readonly SMARTLOCK__COMMON__GOOGLE_API_UNAVAILABLE;
-    readonly SMARTLOCK__COMMON__RESOLUTION_PROMPT_FAIL;
+  /**
+   * @description      Request a Credential.
+   *                   On Android, if several are found, it will open a modal for user selection.
+   * @returns
+   *                   Credential: if succesfully retrieved.
+   * @errors
+   *                   SMARTLOCK__REQUEST__ACCOUNTS_NOT_FOUND: if there is no credential saved
+   *                   SMARTLOCK__REQUEST__DIALOG_CANCELLED: if user didnt select credential on modal
+   *                   SMARTLOCK__COMMON*
+   */
+  request(): Promise < Credential > ;
 
-    /**
-     * @description      Request a Credential.
-     *                   On Android, if several are found, it will open a modal for user selection.
-     * @returns
-     *                   Credential: if succesfully retrieved.
-     * @errors
-     *                   SMARTLOCK__REQUEST__ACCOUNTS_NOT_FOUND: if there is no credential saved
-     *                   SMARTLOCK__REQUEST__DIALOG_CANCELLED: if user didnt select credential on modal
-     *                   SMARTLOCK__COMMON*
-     */
-    request(): Promise < Credential > ;
+  /**
+   * @description      Saves a Credential.
+   *                   On Android, it may prompt a modal for user authorization.
+   * @param
+   *                   id: Credential identifier.
+   *                   name: User name that will be showm on Request.
+   *                   password: credential password.
+   *                   profileUri: profile image that will be shown on Request.
+   * @returns
+   *                   true: if succesfully saved.
+   * @errors
+   *                   SMARTLOCK__SAVE: if User denied authorization
+   *                   SMARTLOCK__SAVE__BAD_REQUEST: if password is empty
+   *                   SMARTLOCK__COMMON*
+   */
+  save(credential: Credential): Promise < true > ;
 
-    /**
-     * @description      Saves a Credential.
-     *                   On Android, it may prompt a modal for user authorization.
-     * @param
-     *                   id: Credential identifier.
-     *                   name: User name that will be showm on Request.
-     *                   password: credential password.
-     *                   profileUri: profile image that will be shown on Request.
-     * @returns
-     *                   true: if succesfully saved.
-     * @errors
-     *                   SMARTLOCK__SAVE: if User denied authorization
-     *                   SMARTLOCK__SAVE__BAD_REQUEST: if password is empty
-     *                   SMARTLOCK__COMMON*
-     */
-    save(credential: Credential): Promise < true > ;
-
-    /**
-     * @description      Deletes a Credential.
-     * @param
-     *                   id: Credential identifier.
-     * @returns
-     *                   true: if succesfully deleted.
-     * @errors
-     *                   SMARTLOCK__DELETE: Couldnt find Credential
-     *                   SMARTLOCK__COMMON*
-     */
-    delete(credentialDeleteRequest: CredentialDeleteRequest): Promise < true > ;
-  }
-
-  interface Credential {
-    id: string;
-    name: string;
-    password: string;
-    profileUri ? : string;
-  }
-
-  interface CredentialDeleteRequest {
-    id: string;
-  }
-  
+  /**
+   * @description      Deletes a Credential.
+   * @param
+   *                   id: Credential identifier.
+   * @returns
+   *                   true: if succesfully deleted.
+   * @errors
+   *                   SMARTLOCK__DELETE: Couldnt find Credential
+   *                   SMARTLOCK__COMMON*
+   */
+  delete(credentialDeleteRequest: CredentialDeleteRequest): Promise < true > ;
 }
 
-declare var Smartlock: SmartlockPlugin.SmartlockPluginStatic;
+
+interface Credential {
+  id: string;
+  name: string;
+  password: string;
+  profileUri?: string;
+}
+
+interface CredentialDeleteRequest {
+  id: string;
+}
+
+export { SmartlockPlugin, Credential, CredentialDeleteRequest };
